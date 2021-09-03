057c44f9e956596ce408a97cd682ddd82ad3084d
Sami Reda Almouzani <sra5349@h3gauss.cs.hbg.psu.edu>
2021-09-03 16:26:29 -0400

    changed all my mistakes

--- hw1/hw1.cxx
+++ hw1/hw1.cxx
@@ -20,7 +20,8 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <cstring>
+#include <cstdlib>
 
 struct RGB {
 		float r;
@@ -28,15 +29,21 @@ struct RGB {
 		float b;
 };		
 
-int main(int argc, char* argv[]){
+int main(int argc, char** argv){
 		// retrieve height and width from argumensts and convert them to type int
-		int height = atoi(argv[1]), width = atoi(argv[2]);
 		
+		int height = atoi(argv[1]), width = atoi(argv[2]);
+		//char* cheight = argv[1];
+	        //char* cwidth = argv[2];
 		//creatte an output file stream
+	
 		std::ofstream file;
 		
 		//retrieve the `
 		file.open(argv[3]);
+
+		file << "P3" << "\r\n";
+		file << width << " " << height << "\r\n" << "255" << "\r\n";
 
 			
 		struct RGB **raster =  new RGB*[width];
@@ -44,19 +51,19 @@ int main(int argc, char* argv[]){
 		for(int i = 0; i < width; i++)
 				raster[i] = new RGB[height];
 		
-		file << "p3" << std::endl;
-		file << width << " " << height << "\n" << "255" << std::endl;
-		
+				
 		for(int h = 0; h < height; h++){
 				for(int w = 0; w < width; w++){
 						int col = ((w & 0x08) == 0) ^ ((h & 0x08) == 0);
-						raster[h][w].r = static_cast<float>(col);
-						raster[h][w].g = static_cast<float>(col & 0x00);
-						raster[h][w].b = static_cast<float>(col & 0x11);
+						raster[h][w].r = 255 * static_cast<float>(col);
+						raster[h][w].g = 255 * static_cast<float>(col & 0x00);
+						raster[h][w].b = 255 * static_cast<float>(col & 0x11);
 						
 						file << raster[h][w].r << " " << raster[h][w].g << " " << raster[h][w].b << " "; 
 				}
+				file << "\r\n";
 		}
+
 
 		
 		return 0;
